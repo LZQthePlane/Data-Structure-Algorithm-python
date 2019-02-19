40c3179f7e9b68bbd5ea2fece752b4bf06b7a48e
Zhiqiang Lee <39208670+LZQthePlane@users.noreply.github.com>
2019-02-19 15:42:29 +0800

    Add insertion_sort, shell_sort, selection_sort

--- Sort.py
+++ Sort.py
@@ -19,10 +19,73 @@ def bubble_sort(my_list):
 
 
 def insertion_sort(my_list):
-    pass
+    """
+    插入排序：类似于抓牌的方法
+    优点：同样不会破坏相同大小元素的相对位置，比较稳定；对于基本有序的序列，排序速度很快
+    缺点：O(N^2)的复杂度，较大
+    """
+    list_length = len(my_list)
+    # 默认已经有一张牌（一个元素），从第二个开始循环
+    for i in range(1, list_length):
+        # tmp为新抓的牌
+        tmp = my_list[i]
+        # 如果新抓的牌比已有的最后一个元素（最大元素）大，则无需进行排序
+        if my_list[i] < my_list[i-1]:
+            # 初始化插入位置
+            insert_idx = 0
+            # 从后往前循环，比较大小
+            for j in range(i-1, -1, -1):
+                # 如果新牌小于当前元素，将当前元素往后移，并更新插入位置
+                if tmp < my_list[j]:
+                    my_list[j+1] = my_list[j]
+                    insert_idx = j
+                # 否则跳出循环
+                else:
+                    break
+            # 将新牌tmp插入保存的插入位置
+            my_list[insert_idx] = tmp
+    return my_list
+
+
+def shell_sort(my_list):
+    """
+    希尔排序：分组插入排序,插入排序的改进版
+    定义一个增量序列（逐渐减小直至为1），按照序列中元素的大小，间隔地进行插入排序
+    插入排序一般来说是低效的，因为插入排序每次只能将数据移动一位
+    缺点：非稳定
+    """
+    list_length = len(my_list)
+    gap = list_length // 2
+    while gap > 0:
+        for i in range(gap, list_length):
+            # 间隔地进行插入排序，这里用的是比较大小交换位置的方法
+            # 上述的插入排序中用的是比较并向后移位的方法
+            # 看视频更易理解
+            while i > gap and my_list[i] < my_list[i-gap]:
+                my_list[i], my_list[i-gap] = my_list[i-gap], my_list[i]
+                i -= gap
+        gap //= 2
+    return my_list
+
+
+def selection_sort(my_list):
+    """
+    选择排序：最简单直观的排序
+    在未排序序列中找到最小元素，存放到排序序列的起始位置。再从剩余未排序元素中继续寻找最小元素，然后放到已排序序列的末尾
+    优点：简单，稳定
+    缺点：O(N^2)的复杂度
+    """
+    list_length = len(my_list)
+    for i in range(list_length-1):
+        min_idx = i
+        for j in range(i+1, list_length):
+            if my_list[j] < my_list[min_idx]:
+                min_idx = j
+        my_list[i], my_list[min_idx] = my_list[min_idx], my_list[i]
+    return my_list
 
 
 test_list = [1, 5, 3, 7, 9, 6, 4, 2, 8]
-result = bubble_sort(test_list)
+result = selection_sort(test_list)
 for item in result:
     print(item)
