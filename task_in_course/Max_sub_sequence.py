e5895c1e041081414282fd7612d1ad813f724e40
Zhiqiang Lee <39208670+LZQthePlane@users.noreply.github.com>
2019-05-29 20:52:37 +0800

    Create Max_sub_sequence.py

---
+++ task_in_course/Max_sub_sequence.py
@@ -0,0 +1,17 @@
+def max_subseq_sum(my_list):
+    """在线处理方法"""
+    num = len(my_list)
+    this_sum = max_sum = 0
+    for i in range(num):
+        this_sum += my_list[i]
+        if this_sum > max_sum:
+            max_sum = this_sum
+        # 如果当前的子列和this_sum小于0，就置为0
+        # 说明其无法使后续部分的和增大，舍弃之
+        elif this_sum < 0:
+            this_sum = 0
+    return max_sum
+
+
+l = [-1, 3, -2, 4, -6, 1, 6, -1]
+print(max_subseq_sum(l))
