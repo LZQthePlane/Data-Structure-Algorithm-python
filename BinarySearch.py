b2f8f6fd28256feb3d92181dfbef56045acab796
Zhiqiang Lee <39208670+LZQthePlane@users.noreply.github.com>
2020-03-20 11:02:25 +0800

    Update BinarySearch.py
    Template of BinarySearch

--- BinarySearch.py
+++ BinarySearch.py
@@ -1,14 +1,14 @@
 def binary_search(sort_list, data):
     left = 0
     right = len(sort_list) - 1
-    while left <= right:
-        mid = (left + right) // 2
+    while left < right:
+        mid = left + (right - left) // 2
         if sort_list[mid] < data:
-            left = mid + 1              # 一定要+1或者-1，
-        elif sort_list[mid] > data:     # 否则会导致循环在未找到data情况下 无法跳出
-            right = mid - 1
-        else:
-            return mid
+            left = mid + 1              # 一定要+1或者-1，否则会导致循环在未找到data情况下 无法跳出
+        else: 
+            right = mid
+    if sort_list[left] == data:
+        return left
     return 'not found'
 
 
