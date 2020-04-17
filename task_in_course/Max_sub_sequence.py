fc907c27bd55d422ba712ff792d74f74a246e079
Zhiqiang Lee <39208670+LZQthePlane@users.noreply.github.com>
2020-04-17 11:15:03 +0800

    Update Max_sub_sequence.py

--- task_in_course/Max_sub_sequence.py
+++ task_in_course/Max_sub_sequence.py
@@ -9,7 +9,7 @@ def max_subseq_sum(my_list):
             max_sum = this_sum
         # 如果当前的子列和this_sum小于0，就置为0
         # 说明其无法使后续部分的和增大，舍弃之
-        elif this_sum < 0:
+        if this_sum < 0:
             this_sum = 0
     return max_sum
 
